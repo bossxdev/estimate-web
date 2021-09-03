a48ea1c2ef9b9501a2e031b5047306e17d508bea
bossxdev <suphanut_boss@outlook.com>
2021-09-03 17:41:32 +0700

    Refactor getData component.

--- pages/api/getData.api.jsx
+++ pages/api/getData.api.jsx
@@ -1,6 +1,5 @@
-import axios from 'axios'
 import * as EndPoints from '../api/endPoints'
-import getData from './getData'
+import getData from './local'
 
 //? สินค้า
 export const GET_PRODUCT_CATEGORY = async () => {
@@ -9,34 +8,25 @@ export const GET_PRODUCT_CATEGORY = async () => {
 }
 
 //? รูปแบบสินค้า
-export const GET_MATERIAL_CATEGORY = async (token) => {
-  const url = 'http://localhost:8000/api/material/material-category?sort=id:ASC'
-  const result = await axios.get(url, {
-    headers: { Authorization: `Bearer ${token}` },
-  })
+export const GET_MATERIAL_CATEGORY = async () => {
+  const result = await getData.get(EndPoints.MATERIAL + `/material-category`)
   return result.data
 }
 
 //? ประเภทกระดาษ
-export const GET_PAPER_LIST = async (token) => {
-  const url = 'http://localhost:8000/api/material/paper-category?_sort=id:ASC'
-  const result = await axios.get(url, {
-    headers: { Authorization: `Bearer ${token}` },
-  })
+export const GET_PAPER_LIST = async () => {
+  const result = await getData.get(EndPoints.MATERIAL + `/paper-category`)
+  return result.data
+}
+
+//? ประเภทเครื่องพิมพ์
+export const GET_PRINTER_NAME = async () => {
+  const result = await getData.get(EndPoints.MATERIAL + `printer_category`)
   return result.data
 }
 
 //? การเคลือบ
 export const GET_ENAMEL_LIST = async () => {
-  const result = await getData.get('/enamels-price?_sort=id:ASC')
-  return result.data
-}
-
-//? ประเภทเครื่องพิมพ์
-export const GET_PRINTER_NAME = async (token) => {
-  const url = 'http://localhost:8000/api/material/printer-category?_sort=id:ASC'
-  const result = await axios.get(url, {
-    headers: { Authorization: `Bearer ${token}` },
-  })
+  const result = await getData.get(`/enamels-price`)
   return result.data
 }
