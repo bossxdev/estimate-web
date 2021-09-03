6bf4ffa61bcd1182d629c58ec3ac096b2cd04c3d
bossxdev <suphanut_boss@outlook.com>
2021-09-03 16:09:43 +0700

    Interceptors add.

--- pages/api/getData.api.jsx
+++ pages/api/getData.api.jsx
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import { AxiosGetData } from './http'
 
 //? สินค้า
 export const GET_PRODUCT_CATEGORY = async (token) => {
@@ -29,10 +30,12 @@ export const GET_PAPER_LIST = async (token) => {
 
 //? การเคลือบ
 export const GET_ENAMEL_LIST = async (token) => {
-  const url = 'http://localhost:8000/api/enamels-price?_sort=id:ASC'
-  const result = await axios.get(url, {
-    headers: { Authorization: `Bearer ${token}` },
-  })
+  // const url = 'http://localhost:8000/api/enamels-price?_sort=id:ASC'
+  // const result = await AxiosGetData.get(url, {
+  //   headers: { Authorization: `Bearer ${token}` },
+  // })
+  const result = await AxiosGetData.get('/enamels-price?_sort=id:ASC')
+  console.log('result ------>', result.data)
   return result.data
 }
 
--- pages/api/http.jsx
+++ pages/api/http.jsx
@@ -11,8 +11,8 @@ const Http = axios.create({
     'Access-Control-Allow-Credentials': 'true',
     'Access-Control-Allow-Methods': 'DELETE, POST, GET, PUT, OPTIONS',
     'Access-Control-Allow-Headers':
-      'ManageContent-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With'
-  }
+      'ManageContent-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With',
+  },
 })
 
 Http.interceptors.request.use(
@@ -47,3 +47,96 @@ Http.interceptors.response.use(
 )
 
 export default Http
+
+// for Authen
+const AxiosAuthen = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_ENDPOINT,
+  timeout: 30000,
+  headers: {
+    'Content-Type': 'application/json; charset=UTF-8',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Credentials': 'true',
+    'Access-Control-Allow-Methods': 'DELETE, POST, GET, PUT, OPTIONS',
+    'Access-Control-Allow-Headers':
+      'ManageContent-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With',
+  },
+})
+
+AxiosAuthen.interceptors.request.use(
+  function (config) {
+    const token = Cookies.get('token')
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`
+    }
+    return config
+  },
+  function (error) {
+    return Promise.reject(error)
+  }
+)
+
+AxiosAuthen.interceptors.response.use(
+  function (response) {
+    return response
+  },
+  function (error) {
+    if (
+      error != null &&
+      error.response != null &&
+      error.response.status === HTTP_STATUS_CODE.UNAUTHORIZED
+    ) {
+      Cookies.remove('token')
+      window.location = '/'
+    }
+    return Promise.reject(error)
+  }
+)
+// for Authen
+
+// for get Data
+const AxiosGetData = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_ENDPOINTS,
+  timeout: 30000,
+  headers: {
+    'Content-Type': 'application/json; charset=UTF-8',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Credentials': 'true',
+    'Access-Control-Allow-Methods': 'DELETE, POST, GET, PUT, OPTIONS',
+    'Access-Control-Allow-Headers':
+      'ManageContent-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With',
+  },
+})
+
+AxiosGetData.interceptors.request.use(
+  function (config) {
+    const token = Cookies.get('token')
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`
+    }
+    return config
+  },
+  function (error) {
+    return Promise.reject(error)
+  }
+)
+
+AxiosGetData.interceptors.response.use(
+  function (response) {
+    return response
+  },
+  function (error) {
+    if (
+      error != null &&
+      error.response != null &&
+      error.response.status === HTTP_STATUS_CODE.UNAUTHORIZED
+    ) {
+      Cookies.remove('token')
+      window.location = '/'
+    }
+    return Promise.reject(error)
+  }
+)
+
+export { AxiosAuthen, AxiosGetData }
